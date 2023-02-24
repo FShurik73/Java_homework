d10ac951d5cda73e9a21ffff67bc2dbdd0e48275
Alexandr Fokin <shurik_f@rambler.ru>
2023-02-24 12:57:01 +0300

    Создал управляющую функцию main

--- Homework4/Task4.java
+++ Homework4/Task4.java
@@ -1,17 +1,15 @@
 package Homework4;
 
 public class Task4 {
-    public void sort(int arr[])
-    {
+    public void sort(int arr[]) {
         int n = arr.length;
 
         // Построение кучи (перегруппируем массив)
         for (int i = n / 2 - 1; i >= 0; i--)
             heapify(arr, n, i);
 
-        // Один за другим извлекаем элементы из кучи   
-        for (int i=n-1; i>=0; i--)
-        {
+        // Один за другим извлекаем элементы из кучи
+        for (int i = n - 1; i >= 0; i--) {
             // Перемещаем текущий корень в конец
             int temp = arr[0];
             arr[0] = arr[i];
@@ -21,30 +19,41 @@ public class Task4 {
             heapify(arr, i, 0);
         }
     }
-        // Процедура для преобразования в двоичную кучу поддерева с корневым узлом i, что является
-// индексом в arr[]. n - размер кучи
-void heapify(int arr[], int n, int i)
-{
-    int largest = i; // Инициализируем наибольший элемент как корень
-    int l = 2*i + 1; // левый = 2*i + 1
-    int r = 2*i + 2; // правый = 2*i + 2
 
-       // Если левый дочерний элемент больше корня
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
+    // Процедура для преобразования в двоичную кучу поддерева с корневым узлом i,
+    // что является
+    // индексом в arr[]. n - размер кучи
+    void heapify(int arr[], int n, int i) {
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int l = 2 * i + 1; // левый = 2*i + 1
+        int r = 2 * i + 2; // правый = 2*i + 2
 
-      // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
-   // Если самый большой элемент не корень
-    if (largest != i)
-    {
-        int swap = arr[i];
-        arr[i] = arr[largest];
-        arr[largest] = swap;
+        // Если левый дочерний элемент больше корня
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
 
-      // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
-        heapify(arr, n, largest);
+        // Если правый дочерний элемент больше, чем самый большой элемент на данный
+        // момент
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        // Если самый большой элемент не корень
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(arr, n, largest);
+        }
     }
-}
+
+    // Управляющая программа
+    public static void main(String args[]) {
+        int arr[] = { 8, 43, -3, 5, 239, 6, 9, -376, 8, -4, 2, -99, 0, 12, 935, -10, 7, };
+        Task4 ob = new Task4();
+        ob.sort(arr);
+
+        System.out.println("Отсортированный массив " + java.util.Arrays.toString(arr));
+        
+    }
 }
