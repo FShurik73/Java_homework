66be1ac98c0a43b2f07bdb4ebf60ab9324f56d4b
Alexandr Fokin <shurik_f@rambler.ru>
2023-03-01 16:58:23 +0300

    Создал класс WaveAlgoritm и метод main

--- Homework5/Task5.java
+++ Homework5/Task5.java
@@ -1,12 +1,19 @@
 package Homework5;
 
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Task5 {
     public static void main(String[] args) {
         var mg = new MapGenerator();
         System.out.println(
             new MapPrinter().mapColor(mg.getMap())
+        );
+
+        var lee = new WaveAlgoritm(mg.getMap());
+        lee.Colorize(new Point2D(3, 3));
+        System.out.println(
+            new MapPrinter().rawData(mg.getMap())
         );
     }
 }
@@ -113,5 +120,39 @@ class MapPrinter {
             sb.append("\n");   
         }
         return sb.toString();
+    }
+}
+class WaveAlgoritm {
+    int[][] map;
+
+    public WaveAlgoritm(int[][] map) {
+        this.map = map;
+    }
+
+    public void Colorize(Point2D startPoint) {
+        Queue<Point2D> queue = new LinkedList<Point2D>();
+        queue.add(startPoint);
+        map[startPoint.x][startPoint.y] = 1;
+
+        while (queue.size() != 0) {
+            Point2D p = queue.remove();
+
+            if (map[p.x - 1][p.y] == 0) {
+                queue.add(new Point2D(p.x - 1, p.y));
+                map[p.x - 1][p.y] = map[p.x][p.y] + 1;
+            }
+            if (map[p.x][p.y - 1] == 0) {
+                queue.add(new Point2D(p.x, p.y - 1));
+                map[p.x][p.y - 1] = map[p.x][p.y] + 1;
+            }
+            if (map[p.x + 1][p.y] == 0) {
+                queue.add(new Point2D(p.x + 1, p.y));
+                map[p.x + 1][p.y] = map[p.x][p.y] + 1;
+            }
+            if (map[p.x][p.y + 1] == 0) {
+                queue.add(new Point2D(p.x, p.y + 1));
+                map[p.x][p.y + 1] = map[p.x][p.y] + 1;
+            }
+        }
     }
 }
