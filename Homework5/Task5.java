9dc7a97d7e6b7f9ff7acd6fce17d917dbbab566c
Alexandr Fokin <shurik_f@rambler.ru>
2023-03-04 22:20:11 +0300

    Добавлен поиск кратчайшего пути с распечаткой двух вариантов карт

--- Homework5/Task5.java
+++ Homework5/Task5.java
@@ -15,6 +15,15 @@ public class Task5 {
         System.out.println(
             new MapPrinter().rawData(mg.getMap())
         );
+
+        var exi = new WaveAlgoritm(mg.getMap());
+        exi.getRoad(new Point2D(15, 21));
+        System.out.println(
+            new MapPrinter().mapColor(mg.getMap())
+        );
+        System.out.println(
+            new MapPrinter().rawData(mg.getMap())
+        );
     }
 }
 
@@ -99,16 +108,16 @@ class MapPrinter {
            for (int col = 0; col < map[row].length; col++) {
             switch (map[row][col]) {
                 case 0:
-                    sb.append("-");
+                    sb.append("--");
                     break;
                 case -1:
-                    sb.append("▓");
+                    sb.append("▓▓");
                     break;
-                case -2:
-                    sb.append("K");
+                case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33:
+                    sb.append("--");
                     break;
                 case -3:
-                    sb.append("E");
+                    sb.append("<>");
                     break;    
                 default:
                     break;
@@ -122,6 +131,7 @@ class MapPrinter {
         return sb.toString();
     }
 }
+
 class WaveAlgoritm {
     int[][] map;
 
@@ -152,6 +162,34 @@ class WaveAlgoritm {
             if (map[p.x][p.y + 1] == 0) {
                 queue.add(new Point2D(p.x, p.y + 1));
                 map[p.x][p.y + 1] = map[p.x][p.y] + 1;
+            }
+        }
+    }
+
+    public void getRoad(Point2D exitPoint) {
+        
+        Queue<Point2D> road = new LinkedList<>();
+        road.add(exitPoint);
+        map[exitPoint.x][exitPoint.y] = 31;
+
+        while (road.size() != 0) {
+            Point2D p1 = road.remove();
+
+            if (map[p1.x - 1][p1.y] == map[p1.x][p1.y] - 1) {
+                road.add(new Point2D(p1.x - 1, p1.y));
+                map[p1.x][p1.y] = -3;
+            }
+            if (map[p1.x][p1.y - 1] == map[p1.x][p1.y] - 1) {
+                road.add(new Point2D(p1.x, p1.y - 1));
+                map[p1.x][p1.y] = -3;
+            }
+            if (map[p1.x + 1][p1.y] == map[p1.x][p1.y] - 1) {
+                road.add(new Point2D(p1.x + 1, p1.y));
+                map[p1.x][p1.y] = -3;
+            }
+            if (map[p1.x][p1.y + 1] == map[p1.x][p1.y] - 1) {
+                road.add(new Point2D(p1.x, p1.y + 1));
+                map[p1.x][p1.y] = -3;
             }
         }
     }
